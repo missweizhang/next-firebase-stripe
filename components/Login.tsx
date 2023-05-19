6136882e8203bf0c67000e78fffc0a985e64fc93
Wei Zhang <missweizhang@gmail.com>
2023-05-19 06:59:04 -0700

    add sign in error handling

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,23 +1,20 @@
 "use client";
 
 import { ReactElement } from "react";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
+import { User } from "firebase/auth";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { db, auth } from "@/firebase/firebaseClient";
 import StyledButton from "./StyledButton";
 
 interface Props {}
 
 const Login = ({}: Props): ReactElement => {
-  const signInWithGoogle = async () => {
-    const { user } = await signInWithPopup(auth, new GoogleAuthProvider());
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
 
-    if (!user) {
-      throw new Error("Cannot login");
-    }
-
-    // store user credentials in firestore collection 'users'
-    await setDoc(doc(db, "users", user?.uid), {
+  // store user credentials in firestore collection 'users'
+  const addUserToFirestore = async (user: User) => {
+    await setDoc(doc(db, "users", user.uid), {
       uid: user.uid,
       email: user.email,
       displayName: user.displayName,
@@ -26,11 +23,18 @@ const Login = ({}: Props): ReactElement => {
     });
   };
 
+  if (user?.user) {
+    addUserToFirestore(user.user);
+  }
+
   return (
     <div>
       <StyledButton onClick={() => signInWithGoogle()}>
         Sign in with Google
       </StyledButton>
+      {!loading && error && (
+        <p className="text-red-600 my-1">Error: {error.message}</p>
+      )}
     </div>
   );
 };
